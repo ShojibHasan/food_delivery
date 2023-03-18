8946c885171c83e04e90a685c00ff09bab8c412c
shojibhasan <shojibhasan15@gmail.com>
2023-03-18 22:43:21 +0600

    django signals

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,9 +4,11 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     list_display = ('email','first_name','last_name','username','role','is_active')
+    list_display_links = ('email','first_name','last_name','username','role','is_active')
     ordering = ('-date_joined',)
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
 
 admin.site.register(User,CustomUserAdmin)
+admin.site.register(UserProfile)
---
+++ accounts/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save,pre_save
+from django.dispatch import receiver
+from .models import User,UserProfile
+
+
+@receiver(post_save,sender=User)
+def post_save_create_profile_receiver(sender,instance,created,**kwargs):
+    print("This is created: ",created)
+    if created:
+        UserProfile.objects.create(user=instance)
+        print("User Profile Created")
+    else:
+        try:
+            print("Else")
+            profile = UserProfile.objects.get(user = instance)
+            profile.save()
+        
+        except:
+            UserProfile.objects.create(user=instance)
+            print("Profile Not exist, but create a new one")
+            
+@receiver(pre_save,sender=User)
+def pre_save_create_profile_reciver(sender,instance,**kwargs):
+    print(instance.username,"This user is being saved")
